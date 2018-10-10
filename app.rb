f842f438494e4c64179dba0d1687d26004ed1aa7
Z1mm3r <czimmer@ramapo.edu>
2018-10-10 10:14:58 -0400

    Done.

--- app.rb
+++ app.rb
@@ -5,5 +5,29 @@ module FormsLab
 
     # code other routes/actions here
 
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :"/pirates/new"
+    end
+
+    post '/pirates' do
+
+      @pirate = Pirate.new
+      @pirate.name = params["pirate"]["name"]
+      @pirate.weight = params["pirate"]["weight"]
+      @pirate.height = params["pirate"]["height"]
+
+      params["pirate"]["ships"].each do |ship|
+        @ship = Ship.new
+        @ship.name = ship["name"]
+        @ship.type = ship["type"]
+        @ship.booty = ship["booty"]
+      end
+
+      erb :"/pirates/show"
+    end
   end
 end
